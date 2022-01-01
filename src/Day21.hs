be74bf099e1ee492195a7e463cb54c3bb5d446c7
Bruno Iljazovic <bruno.iljazovic@gmail.com>
2022-01-01 20:15:47 +0100

    day 21

--- src/Day21.hs
+++ src/Day21.hs
@@ -19,14 +19,12 @@ addWithRollOver :: Int -> Int -> Int -> Int
 addWithRollOver n x = pred >>> (+x) >>> (`mod` n) >>> succ
 
 data GameState = GameState { pos :: (Int, Int),
-                             points :: (Int, Int),
-                             turn :: Int }
+                             points :: (Int, Int) }
 
 step :: Int -> GameState -> GameState
-step roll GameState { pos=(pos1, pos2), points=(points1, points2), turn } =
+step roll GameState { pos=(pos1, pos2), points=(points1, points2) } =
   GameState { pos = (pos2, newPos),
-              points = (points2, points1 + newPos),
-              turn = addWithRollOver 2 1 turn }
+              points = (points2, points1 + newPos) }
   where
     newPos = addWithRollOver mapSize roll pos1
 
@@ -48,21 +46,22 @@ dieSizeB = 3
 deriveMemoizable ''GameState
 
 solveB :: GameState -> V2 Integer
-solveB = memoFix $ \go gameState
- -> sum $ do
-  roll <- sum <$> replicateM 3 [1..dieSizeB]
-  let gameState' = step roll gameState
-  return $ if playerWon pointsLimitB gameState'
-     then case turn gameState' of
-            1 -> V2 1 0
-            2 -> V2 0 1
-     else go gameState'
+solveB = go' (1::Int)
+  where 
+    go' = memoFix2 $ \go turn gameState 
+           -> sum $ do
+            roll <- sum <$> replicateM 3 [1..dieSizeB]
+            let gameState' = step roll gameState
+            return $ if playerWon pointsLimitB gameState'
+               then case turn of
+                      1 -> V2 1 0
+                      2 -> V2 0 1
+               else go (3-turn) gameState'
 
 main21 :: IO ()
 main21 = do
     input <- Map.fromList . mapMaybe parse . lines <$> readFile "res/input21"
     let initialState = GameState { pos = (input Map.! 1, input Map.! 2),
-                                   points = (0, 0),
-                                   turn = 1 }
+                                   points = (0, 0) }
     print $ solveA initialState
     print $ maximum $ solveB initialState
